3685738c6ecdf4af8189221c89da7251f7c7a62e
SelamawitH <selamawitseble@gmail.com>
2021-04-16 12:25:18 -0700

    initial commit

---
+++ newproj12/src/module-info.java
@@ -0,0 +1,2 @@
+module newproj12 {
+}
---
+++ newproj12/src/www/newproj12/project12.java
@@ -0,0 +1,12 @@
+package www.newproj12;
+
+public class project12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello mojo");
+		
+	}
+
+}
